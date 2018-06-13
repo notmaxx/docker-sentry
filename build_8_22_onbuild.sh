597b557d183f7ce22270fc451e5ac69ae4c80266
notmaxx <alexey.pikin@gmail.com>
2018-06-13 15:18:55 -0700

    rebuild base image as well

--- build_8_22_onbuild.sh
+++ build_8_22_onbuild.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
+docker build --rm -t quay.io/workato/sentry:8.22 8.22
+docker push quay.io/workato/sentry:8.22
+
 docker build --rm -t quay.io/workato/sentry:8.22-onbuild 8.22/onbuild
 docker push quay.io/workato/sentry:8.22-onbuild
